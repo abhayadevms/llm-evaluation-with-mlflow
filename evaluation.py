25b09e109da7f062578c2e0e11663491aa792d99
Abhayadev <abhayadev.ms@kennedysiq.com>
2025-06-25 09:14:31 +0100

    llm evaluation

---
+++ evaluation.py
@@ -0,0 +1,53 @@
+import mlflow
+import openai
+import os
+import pandas as pd
+from getpass import getpass
+
+eval_data = pd.DataFrame(
+    {
+        "inputs": [
+            "What is MLflow?",
+            "What is Spark?",
+        ],
+        "ground_truth": [
+            "MLflow is an open-source platform for managing the end-to-end machine learning (ML) "
+            "lifecycle. It was developed by Databricks, a company that specializes in big data and "
+            "machine learning solutions. MLflow is designed to address the challenges that data "
+            "scientists and machine learning engineers face when developing, training, and deploying "
+            "machine learning models.",
+            "Apache Spark is an open-source, distributed computing system designed for big data "
+            "processing and analytics. It was developed in response to limitations of the Hadoop "
+            "MapReduce computing model, offering improvements in speed and ease of use. Spark "
+            "provides libraries for various tasks such as data ingestion, processing, and analysis "
+            "through its components like Spark SQL for structured data, Spark Streaming for "
+            "real-time data processing, and MLlib for machine learning tasks",
+        ],
+    }
+)
+
+with mlflow.start_run() as run:
+    system_prompt = "Answer the following question in two sentences"
+    # Wrap "gpt-4" as an MLflow model.
+    logged_model_info = mlflow.openai.log_model(
+        model="gpt-4",
+        task=openai.chat.completions,
+        name="model",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": "{question}"},
+        ],
+    )
+
+    # Use predefined question-answering metrics to evaluate our model.
+    results = mlflow.evaluate(
+        logged_model_info.model_uri,
+        eval_data,
+        targets="ground_truth",
+        model_type="question-answering",
+    )
+    print(f"See aggregated evaluation results below: \n{results.metrics}")
+
+    # Evaluation result for each data record is available in `results.tables`.
+    eval_table = results.tables["eval_results_table"]
+    print(f"See evaluation table below: \n{eval_table}")
